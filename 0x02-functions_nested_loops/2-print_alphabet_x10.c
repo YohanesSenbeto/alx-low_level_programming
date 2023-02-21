6bc840460c27ebf305b6f57d46f2d1fbb4a79e04
YohanesSenbeto <jonicasenbeto@gmail.com>
2023-02-21 05:49:25 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,6 @@
 
 /**
  * print_alphabet_x10 - print alphabet 10 times in lowercase
- * 
  * Description: print alpahebet 10 times in lowercase
  * Return: void
  */
@@ -17,4 +16,4 @@ void print_alphabet_x10(void)
 	_putchar(ch);
 	_putchar('\n');
 	}
-        }
+}
