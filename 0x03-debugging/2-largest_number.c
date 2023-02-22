b5c5b8865ba9174b319c266a2d23c6b64cb4a233
YohanesSenbeto <jonicasenbeto@gmail.com>
2023-02-22 11:41:32 -0800

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -3,34 +3,34 @@
 #include <stdio.h>
 
 /**
- * largest_number - prints the largest of three integers
- * @a: firts integer
- * @b: second integer
- * @c: third integer
- * Return: larget integer
- */
+* largest_number - prints the largest of three integers
+* @a: firts integer
+* @b: second integer
+* @c: third integer
+* Return: larget integer
+*/
 int largest_number(int a, int b, int c)
 {
 
-    a= 972;
-    b= -98;
-    c= 0;
-    int largest;
+a = 972;
+b = -98;
+c = 0;
+int largest;
 
-    if (a > b && a > c)
-    {
-        largest = a;
-    }
-    else if (b > a && b > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (b > a && b > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
 
-    return (largest);
+return (largest);
 
 }
 /**
@@ -40,16 +40,16 @@ int largest_number(int a, int b, int c)
 
 int main(void)
 {
-        int a, b, c;
-        int largest;
+int a, b, c;
+int largest;
 
-        a = 972;
-        b = -98;
-        c = 0;
+a = 972;
+b = -98;
+c = 0;
 
-        largest = largest_number(a, b, c);
+largest = largest_number(a, b, c);
 
-        printf("%d is the largest number\n", largest);
+printf("%d is the largest number\n", largest);
 
-        return (0);
+return (0);
 }
