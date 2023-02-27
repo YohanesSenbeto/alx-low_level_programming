cb2e97ac1cc78672032a118a70cdd683dd679267
YohanesSenbeto <jonicasenbeto@gmail.com>
2023-02-27 08:23:33 -0800

    unction that swaps the values of two integers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _strlen - a function that returns the length of a string
- * @s: char input
- * Return: length of the input string
- */
+* _strlen - a function that returns the length of a string
+* @s: char input
+* Return: length of the input string
+*/
 int _strlen(char *s)
 {
-  int i = 1, sum = 0;
-  char pl = s[0];
+int i = 1, sum = 0;
+char pl = s[0];
 
-  while (pl != '\0')
-  {
-    sum++;
-    pl = s[i++];
-  }
-  return (sum);
+while (pl != '\0')
+{
+sum++;
+pl = s[i++];
+}
+return (sum);
 }
